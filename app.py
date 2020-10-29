06d307d63147c6ce94081ea9c9b997339cb00167
John Elesho <eleshojohno@gmail.com>
2020-10-29 11:50:42 +0100

    Create base.html, index.html in the Template folde  static folder

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project-todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %d>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello, World"
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Could not add Task'
+
+    else:
+        tasks = Todo.query.order_by('date_created').all()
+        return render_template('index.html', tasks= tasks)
 
 
 if __name__ == "__main__":
